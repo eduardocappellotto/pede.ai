ca05f0b3c366c2f0005616d184052258cfbd6e2a
Eduardo <eduardocappellotto@gmail.com>
2023-07-26 18:31:18 -0300

    feat: base firebase connection

---
+++ plugins/firebase.client.ts
@@ -0,0 +1,35 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from "firebase/auth"
+import { getFirestore } from 'firebase/firestore'
+import { getAnalytics } from "firebase/analytics"
+
+export default defineNuxtPlugin(nuxtApp => {
+
+    const { public: config } = useRuntimeConfig();
+
+    const firebaseConfig = {
+        apiKey: config.apiKey,
+        authDomain: config.authDomain,
+        projectId: config.projectId,
+        storageBucket: config.storageBucket,
+        messagingSenderId: config.messagingSenderId,
+        appId: config.appId,
+        measurementId: config.measurementId
+    };
+
+
+    const app = initializeApp(firebaseConfig)
+
+    const analytics = getAnalytics(app)
+    const auth = getAuth(app)
+    const firestore = getFirestore(app)
+
+    nuxtApp.vueApp.provide('auth', auth)
+    nuxtApp.provide('auth', auth)
+
+    nuxtApp.vueApp.provide('firestore', firestore)
+    nuxtApp.provide('firestore', firestore)
+
+    nuxtApp.vueApp.provide('analytics', analytics)
+    nuxtApp.provide('analytics', analytics)
+})
